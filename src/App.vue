15b7cd070d07969a6de61e782953119b94134fef
Enrico Silva <camponogara5@gmail.com>
2020-07-30 22:54:30 -0300

    feat(status of actions in page): add vueprogressbar to show when something is happening
    when the user load other component or some actions that needs to wait and has no handled loaded. Vue
    progressbar will handle

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <!-- set progressbar -->
+    <vue-progress-bar></vue-progress-bar>
     <Navbar v-if="$route.meta.requireAuth"></Navbar>
     <transition name="fade">
       <router-view />
@@ -8,16 +10,48 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Navbar from '@/components/Navbar.vue';
+import Vue from "vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default Vue.extend({
   components: {
-    Navbar
+    Navbar,
+  },
+  mounted() {
+    //  [App.vue specific] When App.vue is finish loading finish the progress bar
+    // @ts-ignore
+    this.$Progress.finish();
+  },
+  created() {
+    //  [App.vue specific] When App.vue is first loaded start the progress bar
+    // @ts-ignore
+    this.$Progress.start();
+    //  hook the progress bar to start before we move router-view
+    this.$router.beforeEach((to, from, next) => {
+      //  does the page we want to go to have a meta.progress object
+      if (to.meta.progress !== undefined) {
+        const meta = to.meta.progress;
+        // parse meta tags
+        // @ts-ignore
+        this.$Progress.parseMeta(meta);
+      }
+      //  start the progress bar
+      // @ts-ignore
+      this.$Progress.start();
+      //  continue to next page
+      next();
+    });
+    //  hook the progress bar to finish after we've finished moving router-view
+    this.$router.afterEach(() => {
+      //  finish the progress bar
+      // @ts-ignore
+      this.$Progress.finish();
+    });
   }
 });
 </script>
 
 <style lang="scss">
-@import "@/styles/_global.scss";
+@import '@/styles/_global.scss';
+$font-sans: Poppins
 </style>
