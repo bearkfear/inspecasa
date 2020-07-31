3069524af9125434c9d24abc0bfb350c21fbcf8b
Enrico Silva <camponogara5@gmail.com>
2020-07-30 23:00:03 -0300

    fix(cliente interface): corrige interface de cliente onde era requisitado ter atributo funcao
    atributo funcao é pertinente somente ao funcionario no caso usuário do sistema. Não é necessário ter
    a funcao no cliente sendo que o mesmo nao interage com o sistema

--- src/types.ts
+++ src/types.ts
@@ -41,7 +41,6 @@ export interface Cliente {
 	sobrenome: string | null;
 	email: string | null;
 	bio: string | null;
-	funcao: string | null;
 	createdAt?: string | null;
 	changedAt?: string | null;
 	urlImg: string | null;
